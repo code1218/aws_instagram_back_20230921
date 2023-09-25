6a69902dfac4ed97dc82b3a3ef49386736909c44
code1218 <skjil1218@gmail.com>
2023-09-25 17:47:51 +0900

    회원가입 완료

--- src/main/java/com/toyproject/instagram/controller/AuthenticationController.java
+++ src/main/java/com/toyproject/instagram/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.toyproject.instagram.controller;
 
 import com.toyproject.instagram.dto.SignupReqDto;
+import com.toyproject.instagram.exception.SignupException;
 import com.toyproject.instagram.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -29,7 +30,7 @@ public class AuthenticationController {
             bindingResult.getFieldErrors().forEach(error -> {
                 errorMap.put(error.getField(), error.getDefaultMessage());
             });
-            return ResponseEntity.badRequest().body(errorMap);
+            throw new SignupException(errorMap);
         }
 
         userService.signupUser(signupReqDto);
