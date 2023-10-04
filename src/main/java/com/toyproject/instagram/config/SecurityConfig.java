5f3b6a9881ab0e292614cc3dc9af7c439f1ce511
code1218 <skjil1218@gmail.com>
2023-10-04 17:46:40 +0900

    인증 로직 완료

--- src/main/java/com/toyproject/instagram/config/SecurityConfig.java
+++ src/main/java/com/toyproject/instagram/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.toyproject.instagram.config;
 
 import com.toyproject.instagram.exception.AuthenticateExceptionEntryPoint;
+import com.toyproject.instagram.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity  // 현재 우리가 만든 Security 설정 정책을 따르겠다.
 @Configuration
@@ -15,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AuthenticateExceptionEntryPoint authenticateExceptionEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -32,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()   // 나머지 모든 요청은
                 .authenticated() // 인증을 받아라
                 .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling()
                 .authenticationEntryPoint(authenticateExceptionEntryPoint);
     }
--- src/main/java/com/toyproject/instagram/service/UserService.java
+++ src/main/java/com/toyproject/instagram/service/UserService.java
@@ -4,6 +4,7 @@ import com.toyproject.instagram.dto.SigninReqDto;
 import com.toyproject.instagram.dto.SignupReqDto;
 import com.toyproject.instagram.entity.User;
 import com.toyproject.instagram.exception.JwtException;
+import com.toyproject.instagram.exception.SignupException;
 import com.toyproject.instagram.repository.UserMapper;
 import com.toyproject.instagram.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -24,9 +31,48 @@ public class UserService {
 
     public void signupUser(SignupReqDto signupReqDto) {
         User user = signupReqDto.toUserEntity(passwordEncoder);
-        Integer executeCount = userMapper.saveUser(user);
-        System.out.println(executeCount);
+
+        Matcher emailMatcher = Pattern.compile("^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]*$")
+                .matcher(signupReqDto.getPhoneOrEmail());
+        Matcher phoneMatcher = Pattern.compile("^[0-9]{11}+$")
+                .matcher(signupReqDto.getPhoneOrEmail());
+
+        if(emailMatcher.matches()) {
+            user.setEmail(signupReqDto.getPhoneOrEmail());
+        }
+        if(phoneMatcher.matches()) {
+            user.setPhone(signupReqDto.getPhoneOrEmail());
+        }
+
+        checkDuplicated(user);
+        userMapper.saveUser(user);
     }
+
+    private void checkDuplicated(User user) {
+        if(StringUtils.hasText(user.getPhone())) {
+            if(userMapper.findUserByPhone(user.getPhone()) != null) {
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put("phone", "이미 사용중인 연락처입니다.");
+                throw new SignupException(errorMap);
+            }
+        }
+        if(StringUtils.hasText(user.getEmail())) {
+            if(userMapper.findUserByEmail(user.getEmail()) != null) {
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put("email", "이미 사용중인 이메일입니다.");
+                throw new SignupException(errorMap);
+            }
+        }
+        if(StringUtils.hasText(user.getUsername())) {
+            if(userMapper.findUserByUsername(user.getUsername()) != null) {
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put("username", "이미 사용중인 사용자이름입니다.");
+                throw new SignupException(errorMap);
+            }
+        }
+
+    }
+
 
     public String signinUser(SigninReqDto signinReqDto) {
         UsernamePasswordAuthenticationToken authenticationToken =
