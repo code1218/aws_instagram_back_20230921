d9197339e1e160871f6e99851f047e682eb50931
code1218 <skjil1218@gmail.com>
2023-10-06 17:52:35 +0900

    이미지 업로드 완료 및 프로필 페이지 디자인 중

--- src/main/java/com/toyproject/instagram/controller/UserController.java
+++ src/main/java/com/toyproject/instagram/controller/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
 
     @GetMapping("/user/{username}")
-    public ResponseEntity<?> getUser(@PathVariable String username) {
+    public ResponseEntity<?> getUser(@PathVariable String username, HttpServletRequest req) {
 
         return ResponseEntity.ok().body(null);
     }
